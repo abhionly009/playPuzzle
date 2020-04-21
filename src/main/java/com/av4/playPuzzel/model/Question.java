a40f7d505272b0fdd6c9caa6449930150f8e549a
abhi <abhionly009@gmail.com>
2020-04-21 20:30:51 +0530

    Added code to create quize and store it into database along with question and answer

--- src/main/java/com/av4/playPuzzel/model/Question.java
+++ src/main/java/com/av4/playPuzzel/model/Question.java
@@ -1,21 +1,28 @@
 package com.av4.playPuzzel.model;
 
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Question {
 
-	@Column(unique = true, nullable = false)
+	@Column( nullable = false)
 	private String question_info;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long quesion_id; 
+	
+	@OneToMany(cascade =CascadeType.ALL )
+	private List<Answer> answer;
 	
 	public String getQuestion_info() {
 		return question_info;
@@ -31,6 +38,14 @@ public class Question {
 
 	public void setQuesion_id(Long quesion_id) {
 		this.quesion_id = quesion_id;
+	}
+
+	public List<Answer> getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(List<Answer> answer) {
+		this.answer = answer;
 	}
 
 	
--- src/main/java/com/av4/playPuzzel/service/QuizeService.java
+++ src/main/java/com/av4/playPuzzel/service/QuizeService.java
@@ -1,24 +1,57 @@
 package com.av4.playPuzzel.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.av4.playPuzzel.model.Answer;
+import com.av4.playPuzzel.model.Question;
 import com.av4.playPuzzel.model.Quize;
 import com.av4.playPuzzel.repository.QuizeRepository;
 
 @Service
 public class QuizeService {
-	
+
 	@Autowired
 	QuizeRepository quizeRepository;
-	
+
 	public Quize createQuize(Quize quize) {
-		
+
 		return quizeRepository.save(quize);
 	}
 
 	public Quize getQuize(String name) {
+
+		Quize quizeResponse = quizeRepository.findQuizeByName(name);
 		
-		return quizeRepository.findQuizeByName(name);
+		if (quizeResponse!=null) {
+			
+		
+
+		List<Question> questions = quizeResponse.getQuestion();
+
+		for (int i = 0; i < questions.size(); i++) {
+
+			Question question = questions.get(i);
+
+			List<Answer> ans = question.getAnswer();
+
+			for (int j = 0; j < ans.size(); j++) {
+
+				ans.get(j).setCorrectAnswer(null);
+
+			}
+
+		}
+		
+		return quizeResponse;
+		}
+		else {
+			return new Quize();	
+		}
+
+		
 	}
 }
